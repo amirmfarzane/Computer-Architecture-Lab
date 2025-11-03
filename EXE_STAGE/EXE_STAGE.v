4d0f04cef631d9f38a7dfa585eb9e6a3df9554e4
mahdiyehseyfi <mahdiyehseyfi.a@gmail.com>
2025-11-03 16:13:05 +0330

    pipe added

--- EXE_STAGE/EXE_STAGE.v
+++ EXE_STAGE/EXE_STAGE.v
@@ -1,5 +1,7 @@
 module EXE_STAGE (
-    // ===== Inputs =====
+    input  wire        clk,
+    input  wire        rst,
+
     input  wire        WB_EN,
     input  wire        MEM_R_EN,
     input  wire        MEM_W_EN,
@@ -11,29 +13,23 @@ module EXE_STAGE (
     input  wire [31:0] Val_Rm,
     input  wire [31:0] Imm,
     input  wire [11:0] Shift_operand,
-    input  wire [31:0] Signed_EX_Imm24,
+    input  wire [23:0] Signed_EX_Imm24,
     input  wire [3:0]  Dest,
-    input  wire [3:0]  Status,
+    input  wire [3:0]  Status,          
 
-    // ===== Outputs =====
     output wire        WB_EN_out,
     output wire        MEM_R_EN_out,
     output wire        MEM_W_EN_out,
     output wire [31:0] ALU_Res,
     output wire [31:0] Val_Rm_out,
-    output wire [3:0]  Dest_out
+    output wire [3:0]  Dest_out,
+    output wire [31:0] Branch_Address,
+    output wire [3:0]  Status_out       
 );
+    wire Val2_Src = MEM_W_EN | MEM_R_EN;
 
-    // ----- Internal signals -----
-    wire Val2_Src;
     wire [31:0] Val2;
-    wire [31:0] Branch_Address;
-
-    // ----- OR gate -----
-    assign Val2_Src = MEM_W_EN | MEM_R_EN;
-
-    // ----- Val2 Generator -----
-    Val2_Gen val2_gen_inst (
+    Val2_Gen val2_gen_i (
         .Val_Rm(Val_Rm),
         .Imm(Imm),
         .Shift_operand(Shift_operand),
@@ -41,27 +37,34 @@ module EXE_STAGE (
         .Val2(Val2)
     );
 
-    // ----- ALU -----
-    ALU alu_inst (
+    wire [3:0] alu_flags; // [N,Z,C,V]
+    ALU alu_i (
         .Val1(Val_Rn),
         .Val2(Val2),
         .EXE_CMD(EXE_CMD),
         .Status(Status),
-        .ALU_Res(ALU_Res)
+        .ALU_Res(ALU_Res),
+        .Flags(alu_flags)
     );
 
-    // ----- Adder -----
-    Adder adder_inst (
+    Adder adder_i (
         .PC(PC),
         .Signed_EX_Imm24(Signed_EX_Imm24),
         .Branch_Address(Branch_Address)
     );
 
-    // ----- Output connections -----
-    assign WB_EN_out   = WB_EN;
-    assign MEM_R_EN_out = MEM_R_EN;
-    assign MEM_W_EN_out = MEM_W_EN;
-    assign Val_Rm_out  = Val_Rm;
-    assign Dest_out    = Dest;
+    Status_Reg psr_i (
+        .clk(clk),
+        .rst(rst),
+        .S(S),
+        .Flags_in(alu_flags),
+        .Status_out(Status_out)
+    );
+
+    assign WB_EN_out     = WB_EN;
+    assign MEM_R_EN_out  = MEM_R_EN;
+    assign MEM_W_EN_out  = MEM_W_EN;
+    assign Val_Rm_out    = Val_Rm;   
+    assign Dest_out      = Dest;
 
 endmodule
---
+++ EXE_STAGE/REG_PIPE_3.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+
+module REG_PIPE_3(
+    input  clk,
+    input  rst,
+
+    input        WB_EN,
+    input        MEM_R_EN,
+    input        MEM_W_EN,
+    input [31:0] ALU_Res,
+    input [31:0] Val_Rm,
+    input [3:0]  Dest,
+
+    output reg        WB_EN_out,
+    output reg        MEM_R_EN_out,
+    output reg        MEM_W_EN_out,
+    output reg [31:0] Val_Rm_out,
+    output reg [3:0]  Dest_out,
+    output reg [31:0] ALU_Res_out
+  
+);
+    
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            Val_Rm_out <= 32'b0;
+            Dest_out <= 32'b0;
+            ALU_Res_out <= 32'b0;
+            WB_EN_out <= 1'b0;
+            MEM_R_EN_out <= 1'b0;
+            MEM_W_EN_out <= 1'b0;
+        end else begin
+            Val_Rm_out <= Val_Rm;
+            Dest_out <= Dest;
+            ALU_Res_out <= ALU_Res;
+            WB_EN_out <=  WB_EN;
+            MEM_R_EN_out <= MEM_R_EN;
+            MEM_W_EN_out <= MEM_W_EN;
+        end
+    end
+    
+endmodule
